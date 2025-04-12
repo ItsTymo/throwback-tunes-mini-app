b38ef91755db971b26a48171806dfee84e012045
ItsTymo <itstymo.eth@gmail.com>
2025-04-11 20:57:02 -0700

    404 Update

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>🎵 Throwback Tunes</h1>
+      <p>Select a decade to get a random hit song!</p>
+    </main>
+  );
+}
