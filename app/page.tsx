4dd1126874accc0f15c24b4979660fc403131200
ItsTymo <itstymo.eth@gmail.com>
2025-04-12 21:51:28 -0700

    Manifest round two

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
+import { sdk } from '@farcaster/frame-sdk';
 
 export default function Home() {
+  useEffect(() => {
+    sdk.actions.ready(); // Tells Warpcast to hide the splash screen
+  }, []);
+
   return (
     <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
       <h1>🎵 Throwback Tunes</h1>
-      <p>Select a decade to get a random hit song!</p>
+      <p>Click the frame to get a random tune!</p>
     </main>
   );
 }
